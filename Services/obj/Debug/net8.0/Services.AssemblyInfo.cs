29c5d34c94a950ac3235204609ad16eb5ab133ce
Gonza <gonzalomarcos551@gmail.com>
2025-05-24 21:00:13 -0300

    script sql

--- Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
+++ Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6dd830abdb939ffc02e95848454e5d09aa2ac158")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8696e34f6906d6696f95201fd87b087dacb06de9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
