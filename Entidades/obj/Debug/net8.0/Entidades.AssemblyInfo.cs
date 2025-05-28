0caaad1d5ae8dc50922aa677911cc959d7acf31d
Gonza <gonzalomarcos551@gmail.com>
2025-05-28 14:54:12 -0300

    asd

--- Entidades/obj/Debug/net8.0/Entidades.AssemblyInfo.cs
+++ Entidades/obj/Debug/net8.0/Entidades.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entidades")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85e30d25280f00ac3a0ef388c1fcec72fb707a19")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75562625ed4a20c30a54aafbb01724c674a398de")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entidades")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entidades")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
+++ Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68769af7fafc75b85ee02f078ea12b20bafeb89f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75562625ed4a20c30a54aafbb01724c674a398de")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
