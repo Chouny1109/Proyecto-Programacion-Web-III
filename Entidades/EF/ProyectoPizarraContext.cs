ffeae81a62cf9f7cc9f3de5ef01d5150ce818737
lautaro <lautarogerezz12@gmail.com>
2025-06-14 00:30:07 -0300

    imagen en pizarra

--- Entidades/EF/ProyectoPizarraContext.cs
+++ Entidades/EF/ProyectoPizarraContext.cs
@@ -41,6 +41,8 @@ public partial class ProyectoPizarraContext : IdentityDbContext<IdentityUser>
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseSqlServer("Server=.;Database=ProyectoPizarra;Trusted_Connection=True;TrustServerCertificate=True");
 
+    //"Server=.\\SQLEXPRESS;Database=ProyectoPizarra;Trusted_Connection=True;TrustServerCertificate=True"
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
